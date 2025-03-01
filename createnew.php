851b2feb04d11208c0b95965e868fbcbca87809b
Eski <noah.king98@gmail.com>
2025-03-01 02:39:47 -0500

    Add files via upload

--- createnew.php
+++ createnew.php
@@ -70,18 +70,12 @@ $target_file = $target_dir . basename($_FILES["file"]["name"]);
               
                   // Allow certain file formats
            
-              
+                  move_uploaded_file($_FILES["file"]["name"], $target_file)
                   // Check if $uploadOk is set to 0 by an error
                   if ($uploadOk == 0) {
                       echo "Sorry, your file was not uploaded.";
-                  // If everything is ok, try to upload file
-                  } else {
-                      if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-                          echo "The file ". htmlspecialchars(basename($_FILES["file"]["name"])). " has been uploaded.";
-                      } else {
-                          echo "Sorry, there was an error uploading your file.";
-                      }
                   }
+                  
 $baseDir = $target_dir;
 $file = (string)($target_file) ;
 
